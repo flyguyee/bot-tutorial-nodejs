581f79c24d36b7a5011ef756d5d5adaa161c5276
Salvatore <42879063+flyguyee@users.noreply.github.com>
2018-09-01 11:48:49 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,10 +72,10 @@ function postMessage() {
    }
   };
   
-  body = {'message': {
-            'source_guid' : 'rand',
-            'text' : botResponse
-         } 
+  body = {"message": "{
+            "source_guid" : "rand",
+            "text" : botResponse
+         }" 
   };
 
   console.log('sending ' + botResponse + ' to ' + botID);
